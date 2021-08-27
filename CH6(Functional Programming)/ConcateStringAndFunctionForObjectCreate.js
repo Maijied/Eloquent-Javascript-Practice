b41eb58245b3b6345ed02a9fc7f99899724ef93c
unknown <mdshuvo40@gmail.com>
2021-08-27 20:53:53 +0600

    Solved render html problem

--- CH6(Functional Programming)/ConcateStringAndFunctionForObjectCreate.js
+++ CH6(Functional Programming)/ConcateStringAndFunctionForObjectCreate.js
@@ -56,6 +56,49 @@ function escapeHTML(text){
 }
 console.log(escapeHTML("Hello>")); //Hello&gt;
 
+//Turn an HTML element object into string
+function renderHTML(element){
+    var pieces = [];
+
+    function renderAttributes(attributes){
+        var result = [];
+        if(attributes){
+            for(var name in attributes){
+                result.push(" " + name + "=\"" + escapeHTML(attributes[name]) + "\"");
+            } 
+        }
+        return result.join("");
+    }
+    function render(element){
+        //text node
+        if(typeof element == "string"){
+            pieces.push(escapeHTML(element));
+        }
+        //Emoty Tag
+        else if(!element.content || element.content.length == 0){
+            pieces.push("<" + element.name + renderAttributes(element.attributes) + "/>");
+        }
+        //Tag with content
+        else{
+            pieces.push("<" + element.name + renderAttributes(element.attributes) + ">");
+            var elementContent = [element.content];
+            console.log(elementContent);
+            elementContent.forEach(render => {
+                pieces.push("<" + element.name + ">");     
+            });
+            // forEach(element.content, render);
+            // pieces.push("<" + element.name + ">");
+        }
+    }
+    render(element);
+    return pieces.join("");
+}
+console.log(renderHTML(link("http://nedroid.com", "Drawings!")));
+/*
+[ { href: 'http://nedroid.com' } ]
+<a 0="Drawings!"><a>
+*/
+
 //   function htmlDoc(title, bodyContent){
 //       return tag("html", [tag("head", [tag("title", [title])]),
 //              tag("body", bodyContent)]);
